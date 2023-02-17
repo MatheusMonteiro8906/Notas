69d199dd10a55c7403b6eba4826d3b89079545ec
Matheus Monteiro <41351829+MatheusMonteiro8906@users.noreply.github.com>
2023-02-17 13:05:56 -0300

    Delete Notas directory

--- Notas/js/View/View.js
+++
@@ -1,9 +0,0 @@
-class View{
-    constructor(seletor){
-        this.$seletor = blg.$(seletor);
-    }
-    
-    getTemplate(){
-        throw new Error ('metodo getTemplate deve ser implementado pelas classes que herdam View');
-    }
-}
